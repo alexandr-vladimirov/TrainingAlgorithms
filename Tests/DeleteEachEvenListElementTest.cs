8af151e10f32a80a9857a3d8235c0327c3a08878
Aleksandr Vladimirov <aleksandr.vladimirov@akvelon.com>
2016-10-27 10:07:14 +0300

    HaveTwoListsCommonItem and DeleteEachEvenListElement

---
+++ Tests/DeleteEachEvenListElementTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class DeleteEachEvenListElementTest
+    {
+        public Node<T> DeleteEachEvenListElement<T>(Node<T> root)
+        {
+            for (var node = root; node?.Next != null;)
+            {
+                node.Next = node.Next.Next;
+                node = node.Next;
+            }
+
+            return root;
+        }
+
+        [Test]
+        public void EmptyListTest()
+        {
+            Node<int> root = null;
+            root = DeleteEachEvenListElement(root);
+            Assert.IsNull(root);
+        }
+
+        [Test]
+        public void ListWithOneElement()
+        {
+            var root = new Node<int> { Value = 1 };
+            root = DeleteEachEvenListElement(root);
+            Assert.AreEqual(1, root.Value);
+        }
+
+        [Test]
+        public void ListWithTwoElements()
+        {
+            var root = new Node<int> { Value = 1 };
+            root.Next = new Node<int> { Value = 2 };
+            root = DeleteEachEvenListElement(root);
+            Assert.AreEqual(1, root.Value);
+            Assert.IsNull(root.Next);
+        }
+        
+        [Test]
+        public void ListWithThreeElements()
+        {
+            var root = new Node<int> { Value = 1 };
+            root.Next = new Node<int> { Value = 2 };
+            root.Next.Next = new Node<int> { Value = 3 };
+            root = DeleteEachEvenListElement(root);
+            Assert.AreEqual(1, root.Value);
+            Assert.AreEqual(3, root.Next.Value);
+        }
+    }
+}
---
+++ Tests/HaveTwoListsCommonItemsTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class HaveTwoListsCommonItemsTest
+    {
+        public bool HaveTwoListsCommonItem<T>(Node<T> root1, Node<T> root2)
+        {
+            var hashset = new HashSet<T>();
+            for (var node = root1; node != null; node = node.Next)
+                hashset.Add(node.Value);
+
+            for (var node = root2; node != null; node = node.Next)
+                if (hashset.Contains(node.Value))
+                    return true;
+
+            return false;
+        }
+
+        [Test]
+        public void TwoEmptyListsTest()
+        {
+            Node<int> root1 = null;
+            Node<int> root2 = null;
+            Assert.IsFalse(HaveTwoListsCommonItem(root1, root2));
+        }
+
+        [Test]
+        public void EmptyAndNotEmptyListsTest()
+        {
+            Node<int> root1 = null;
+            var root2 = new Node<int> { Value = 1 };
+            Assert.IsFalse(HaveTwoListsCommonItem(root1, root2));
+        }
+
+        [Test]
+        public void ListsWithoutCommonItems()
+        {
+            var root1 = new Node<int> { Value = 1 };
+            root1.Next = new Node<int> { Value = 2 };
+            var root2 = new Node<int> { Value = 10 };
+            root2.Next = new Node<int> { Value = 20 };
+            Assert.IsFalse(HaveTwoListsCommonItem(root1, root2));
+        }
+
+        [Test]
+        public void ListsWithCommonItems()
+        {
+            var root1 = new Node<int> { Value = 1 };
+            root1.Next = new Node<int> { Value = 2 };
+            var root2 = new Node<int> { Value = 10 };
+            root2.Next = new Node<int> { Value = 1 };
+            Assert.IsTrue(HaveTwoListsCommonItem(root1, root2));
+        }
+
+        [Test]
+        public void FirstListWithDuplicatesSecondWithoutCommonItems()
+        {
+            var root1 = new Node<int> { Value = 1 };
+            root1.Next = new Node<int> { Value = 2 };
+            root1.Next.Next = new Node<int> { Value = 2 };
+            var root2 = new Node<int> { Value = 10 };
+            root2.Next = new Node<int> { Value = 20};
+            Assert.IsFalse(HaveTwoListsCommonItem(root1, root2));
+        }
+    }
+}
