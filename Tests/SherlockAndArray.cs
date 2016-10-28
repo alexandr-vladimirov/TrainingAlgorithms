5b916d8e53ab5bedcafa6096db13cf297ec0209e
Aleksandr Vladimirov <aleksandr.vladimirov@akvelon.com>
2016-10-28 17:03:32 +0300

    SherlockAndArray

---
+++ Tests/SherlockAndArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/sherlock-and-array
+    /// </summary>
+    class Solution
+    {
+        public static void Main(String[] args)
+        {
+            var T = int.Parse(Console.ReadLine());
+            for (var t = 0; t < T; t++)
+            {
+                var n = int.Parse(Console.ReadLine());
+                var numbers = Console.ReadLine().Split(' ').ToList().Select(int.Parse).ToArray();
+                var exists = ExistsMiddleElement(numbers);
+                Console.WriteLine(exists ? "YES" : "NO");
+            }
+        }
+
+        public static bool ExistsMiddleElement(int[] numbers)
+        {
+            var leftSum = 0;
+            var rightSum = numbers.Skip(1).Sum();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (leftSum == rightSum)
+                    return true;
+                
+                leftSum += numbers[i];
+                if (i + 1 < numbers.Length)
+                    rightSum -= numbers[i + 1];
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Tests/SherlockAndArrayTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class SherlockAndArrayTest
+    {
+        [TestCase("1 2 3", false)]
+        [TestCase("1 2 3 3", true)]
+        [TestCase("1", true)]
+        [TestCase("1 2", false)]
+        [TestCase("1 1 -1", true)]
+        [TestCase("1 -1 0", true)]
+        [TestCase("1 -2 0", false)]
+        public void ExistsMiddleElement(string numbersLine, bool expectedExists)
+        {
+            var numbers = numbersLine.Split(' ').Select(int.Parse).ToArray();
+            Assert.AreEqual(expectedExists, Solution.ExistsMiddleElement(numbers));
+        }
+    }
+}
