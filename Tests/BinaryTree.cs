1957914d9f9c85cc714377a0b45fdb14d160c635
Aleksandr Vladimirov <aleksandr.vladimirov@akvelon.com>
2016-10-28 10:17:52 +0300

    BinaryTree and IsSearchBinaryTree

---
+++ Tests/BinaryTree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class BinaryTreeNode
+    {
+        public int Value { get; set; }
+        public BinaryTreeNode Left { get; set; }
+        public BinaryTreeNode Right { get; set; }
+    }
+
+    public class BinaryTree
+    {
+        private BinaryTreeNode root;
+
+        public BinaryTree(BinaryTreeNode root)
+        {
+            this.root = root;
+        }
+
+        public bool IsSearchTree()
+        {
+            return IsSearchTree(root, int.MinValue, int.MaxValue);
+        }
+
+        private bool IsSearchTree(BinaryTreeNode node, int min, int max)
+        {
+            if (node == null)
+                return true;
+
+            if (node.Value < min || node.Value > max)
+                return false;
+
+            return IsSearchTree(node.Left, min, node.Value) &&
+                   IsSearchTree(node.Right, node.Value, max);
+        }
+    }
+}
---
+++ Tests/IsSearchBinaryTreeTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class IsSearchBinaryTreeTest
+    {
+        [Test]
+        public void EmptyTree()
+        {
+            var tree = new BinaryTree(null);
+            Assert.IsTrue(tree.IsSearchTree());
+        }
+
+        [Test]
+        public void TreeWithOneNode()
+        {
+            var tree = new BinaryTree(new BinaryTreeNode {Value = 1});
+            Assert.IsTrue(tree.IsSearchTree());
+        }
+
+        [Test]
+        public void BinarySearchTree()
+        {
+            var root = new BinaryTreeNode { Value = 2 };
+            root.Left = new BinaryTreeNode {Value = 1};
+            root.Right = new BinaryTreeNode {Value = 3};
+            var tree = new BinaryTree(root);
+            Assert.IsTrue(tree.IsSearchTree());
+        }
+        
+        [Test]
+        public void BinarySearchTreeWithDuplicates()
+        {
+            var root = new BinaryTreeNode { Value = 2 };
+            root.Left = new BinaryTreeNode {Value = 2};
+            root.Right = new BinaryTreeNode {Value = 2};
+            var tree = new BinaryTree(root);
+            Assert.IsTrue(tree.IsSearchTree());
+        }
+        
+        [Test]
+        public void NotBinarySearchTreeWithDuplicates()
+        {
+            var root = new BinaryTreeNode { Value = 1 };
+            root.Left = new BinaryTreeNode {Value = 2};
+            root.Right = new BinaryTreeNode {Value = 2};
+            var tree = new BinaryTree(root);
+            Assert.IsFalse(tree.IsSearchTree());
+        }
+    }
+}
