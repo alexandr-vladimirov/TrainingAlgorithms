312bfcbbbedf87eb6574f25cc9e391ff54ca17e4
Aleksandr Vladimirov <aleksandr.vladimirov@akvelon.com>
2016-11-27 22:46:00 +0300

    NumberToWordsConverter

---
+++ Tests/NumberToWordsConverter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Tests
+{
+    public enum Culture
+    {
+        American,
+        British
+    }
+
+    public static class NumberToWordsConverter
+    {
+        public static string Convert(int number, Culture culture = Culture.American)
+        {
+            if (number == 0) return "zero";
+            if (number < 0) return "minus " + Convert(-number, culture);
+
+            var words = "";
+            var digitsCount = (int)Math.Truncate(Math.Log10(number)) + 1;
+            var threeDigitBlocksCount = (int)Math.Ceiling(digitsCount / 3.0);
+            for (var blockNumber = threeDigitBlocksCount - 1; blockNumber >= 0; blockNumber--)
+            {
+                var blockValue = GetThreeDigitBlock(number, blockNumber);
+                if (blockValue == 0) continue;
+                var blockWords = ThreeDigitNumberToWords(blockValue, culture);
+                var delimeter = GetBlocksDelimeter(culture, blockNumber, threeDigitBlocksCount, blockValue);
+                words += delimeter + " " + blockWords + " " + blockNames[blockNumber];
+            }
+            return words.Trim();
+        }
+
+        private static string GetBlocksDelimeter(Culture culture, int blockNumber, int blocksCount, int blockValue)
+        {
+            var isHighestBlock = blockNumber == blocksCount - 1;
+            var isLowestBlock = blockNumber == 0;
+            var isMiddleBlock = !isHighestBlock && !isLowestBlock;
+
+            if (isHighestBlock) return "";
+            if (isMiddleBlock) return ",";
+            if (blockValue >= 100) return ",";
+            if (culture == Culture.British) return " and";
+            return "";
+        }
+
+        private static int GetThreeDigitBlock(int number, int blockNumber)
+        {
+            var result = (int)Math.Truncate(number / Math.Pow(1000, blockNumber));
+            result %= 1000;
+            return result;
+        }
+
+        private static string ThreeDigitNumberToWords(int n, Culture culture)
+        {
+            var hundreds = n / 100;
+            var rest = n - hundreds * 100;
+            var hundredWords = hundreds > 0 ? from0To19[hundreds] + " hundred" : "";
+            var delimeter = (culture == Culture.British && hundreds > 0 && rest > 0) ? " and" : "";
+            var restWords = rest > 0 ? " " + DoubleDigitNumberToWords(rest) : "";
+            var words = hundredWords + delimeter + restWords;
+            return words.Trim();
+        }
+
+        private static string DoubleDigitNumberToWords(int n)
+        {
+            if (n <= 19) return from0To19[n];
+            var tens = n / 10;
+            var units = n - tens * 10;
+            var tenWords = tensNames[tens - 2];
+            var unitWords = units > 0 ? "-" + from0To19[units] : "";
+            return tenWords + unitWords;
+        }
+
+        private static string[] blockNames = { "", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion" };
+        private static string[] tensNames = { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+        private static string[] from0To19 =
+        {
+            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+            "eleven", "twelve", "thirteen", "forteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+        };
+    }
+}
---
+++ Tests/NumberToWordsConverterTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class NumberToWordsConverterTest
+    {
+        [TestCase(0, "zero")]
+        [TestCase(1, "one")]
+        [TestCase(-1, "minus one")]
+        [TestCase(10, "ten")]
+        [TestCase(20, "twenty")]
+        [TestCase(22, "twenty-two")]
+        [TestCase(101, "one hundred and one")]
+        [TestCase(1000, "one thousand")]
+        [TestCase(1011, "one thousand and eleven")]
+        [TestCase(1100, "one thousand, one hundred")]
+        [TestCase(1112, "one thousand, one hundred and twelve")]
+        [TestCase(1000001000, "one billion, one thousand")]
+        [TestCase(1234567890, "one billion, two hundred and thirty-four million, five hundred and sixty-seven thousand, eight hundred and ninety")]
+        public void Convert(int number, string britishExpected)
+        {
+            var americanActual = NumberToWordsConverter.Convert(number, Culture.American);
+            var britishActual = NumberToWordsConverter.Convert(number, Culture.British);
+            var americanExpected = britishExpected.Replace(" and", "");
+            Assert.AreEqual(britishExpected, britishActual);
+            Assert.AreEqual(americanExpected, americanActual);
+        }
+    }
+}
