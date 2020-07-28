6f1e7374844d6c283b408e09858e18283976d9bf
Alex Vladimirov <v-alvlad@microsoft.com>
2020-07-28 16:41:23 +0300

    Add AzurePipeline sln

---
+++ AzurePipeline/AzurePipeline/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AzurePipeline")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AzurePipeline")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cd99cc0e-d883-4c1a-9698-79f84622d134")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AzurePipeline/AzurePipeline/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AzurePipeline
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShouldPass()
+        {
+        }
+
+        [TestMethod]
+        public void ShouldFail()
+        {
+            throw new Exception("Azure pipeline test");
+        }
+    }
+}
